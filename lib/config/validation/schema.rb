1c7ed68bb67194e8d37e68e17682ef3b05de7445
German Del Zotto <g.delzotto@stuart.com>
2022-04-26 11:18:26 +0200

    Fix the config/specs problems including constants in module

--- lib/config/validation/schema.rb
+++ lib/config/validation/schema.rb
@@ -1,5 +1,5 @@
-require_relative "../dry_validation_requirements"
-require_relative "../error"
+load 'config/dry_validation_requirements.rb'
+require_relative '../error'
 
 module Config
   module Validation
@@ -13,16 +13,17 @@ module Config
         if block_given?
           begin
             require 'dry/validation/version'
+
             version = Gem::Version.new(Dry::Validation::VERSION)
-            unless ::Config::DRY_VALIDATION_REQUIREMENTS.all? { |req| Gem::Requirement.new(req).satisfied_by?(version) }
+            unless Config::DRY_VALIDATION_REQUIREMENTS.all? { |req| Gem::Requirement.new(req).satisfied_by?(version) }
               raise LoadError
             end
           rescue LoadError
-            raise ::Config::Error, "Could not find a dry-validation version matching requirements" \
-              " (#{::Config::DRY_VALIDATION_REQUIREMENTS.map(&:inspect) * ","})"
+            raise Config::Error, "Could not find a dry-validation version matching requirements" \
+              " (#{Config::DRY_VALIDATION_REQUIREMENTS.map(&:inspect) * ","})"
           end
           # Delay require until optional schema validation is requested
-          require "dry-validation"
+          require 'dry-validation'
           @schema = Dry::Schema.define(&block)
         else
           @schema
